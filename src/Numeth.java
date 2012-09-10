bab8c3b118c8a7efbf167bbd897a3d77c4820b5a
Rafael Rendon Pablo <smart.rendon@gmail.com>
2012-09-10 01:44:45 -0500

    I have added a functional mathematical expression parser.

--- src/Numeth.java
+++ src/Numeth.java
@@ -17,12 +17,31 @@
     along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+import edu.inforscience.lang.Parser;
+
+import java.io.*;
+
 public class Numeth {
 
-  public static void main(String[] args)
+  public static void main(String[] args) throws IOException
   {
-    MainWindow window = new MainWindow();
-    window.setVisible(true);
+    //MainWindow window = new MainWindow();
+    //window.setVisible(true);
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.print(">> ");
+    String expression = reader.readLine();
+    Parser parser = new Parser(expression);
+
+    System.out.println("x\t\tf(x)");
+    for (double x = -5; x <= 5; x += 0.5) {
+      parser.setVariable("x", x);
+      System.out.println(x + "  " + parser.evaluate());
+    }
+
+    //System.out.println(parser.evaluate("2^0/0! - 2^2/2! + 2^4/4! - 2^6/6! + 2^8/8! - 2^10/10!"));
+
   }
 }
 
